083de1f0f66790bc6eccc0b002c0049256c1f24f
Babatunde <babatunde1@Babatundes-MacBook-Pro.local>
2023-01-12 14:35:59 +0000

    Program added

---
+++ Temperature/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Temperature;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Temp_Checker.Temperature();
+
+        Console.ReadKey();
+    }
+}
+
